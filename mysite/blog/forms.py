9bff22629fae994362787fd04dbb466a1d3a1b8b
Camila Moura <camilapaleo@gmail.com>
2018-10-31 20:41:49 +0100

    first commit

---
+++ mysite/blog/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from blog.models import Post,Comment
+
+class PostForm(forms.ModelForm):
+
+    class Meta():
+        model = Post
+        fields = ('author','title', 'text')
+
+        widgets = {
+            'title':forms.TextImput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
+        }
+
+class CommentForm(forms.ModelForm):
+
+    class Meta():
+        model = Comment
+        firlds = ('author', 'text')
+
+        widgets = {
+            'author':forms.TextInput(attrs={'class':'testinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
+        }
