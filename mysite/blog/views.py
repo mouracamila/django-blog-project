fb9bdb0c8a7018684ab1e348d7207008762fab6b
Camila Moura <camilapaleo@gmail.com>
2018-11-02 11:45:51 +0100

    Views, login, post-work in progress. x

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.uti;s import timezone
+from blog.models import Post,Comment
+from blog.forms import PostForm,CommentForm
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (TemplateView,ListView,DetailView,
+                                    CreateView,UpdateView,DelateView)
 
 # Create your views here.
+
+class Aboutview(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date'):
+
+class PostDetail(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_firld_name = 'blog/post_detail.html'
+    form_class =PostForm
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_firld_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostDelateView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_firld_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date_isnnull=True).order_by('created_date')
+
+###################################
+###################################
+
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request,'blog/comment_form.html',{'form':form})
